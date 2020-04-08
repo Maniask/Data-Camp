46dd000c7d8fc1d732e21c01c10f8aeba5cf4276
Mani Agarwal <42671984+Maniask@users.noreply.github.com>
2020-04-08 19:56:13 +0530

    Create script.py

---
+++ Web Scraping with Python/01- Introduction to HTML/02-HTML tree wordy navigation/script.py
@@ -0,0 +1,30 @@
+'''
+HTML tree wordy navigation
+
+html_tree_exercise.png
+
+Given the html tree, which of the following does not correctly navigate to the paragraph element containing "Hello World!"?
+
+Answer the question
+50 XP
+
+Possible Answers
+
+(a). The first child of the body element
+press
+1
+(b). The first child of the second child of the root html element.
+press
+2
+(c). The first sibling of the title element.
+press
+3
+(d). The only sibling of the paragraph p element containing the text "Enjoy DataCamp!".
+press
+4
+(e). The first paragraph element.
+press
+5
+'''
+
+Solution--> (c). The first sibling of the title element. 
