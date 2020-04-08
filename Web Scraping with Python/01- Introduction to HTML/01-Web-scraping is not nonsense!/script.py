5550e016c67329116bb694e728c8a2771e659fb2
Mani Agarwal <42671984+Maniask@users.noreply.github.com>
2020-04-08 19:24:32 +0530

    Create script.py

---
+++ Web Scraping with Python/01- Introduction to HTML/01-Web-scraping is not nonsense!/script.py
@@ -0,0 +1,25 @@
+'''
+Web-scraping is not nonsense!
+
+Select which of the following is not correct.
+
+Answer the question
+50 XP
+
+Possible Answers
+
+(a). Web scraping can be useful for looking through product reviews to gauge public opinion about a particular product.
+press
+1
+(b). Web scraping can be useful for reading through social media posts between users in different areas to compare different language usage.
+press
+2
+(c). Web scraping can be useful for going through online news publications to pick out articles discussing a particular topic.
+press
+3
+(d). None of the above, everyone believes web scraping is nonsense!
+press
+4
+'''
+
+Solution is-->  (d). None of the above, everyone believes web scraping is nonsense!
