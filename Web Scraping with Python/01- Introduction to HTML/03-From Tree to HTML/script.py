7b4a7683470e668ed5705c9a3c4001e68961e392
Mani Agarwal <42671984+Maniask@users.noreply.github.com>
2020-04-08 20:08:54 +0530

    Create script.py

---
+++ Web Scraping with Python/01- Introduction to HTML/03-From Tree to HTML/script.py
@@ -0,0 +1,28 @@
+'''
+From Tree to HTML
+
+Here you are given the chance to create your own bit of HTML code (as a python string). More specifically, below is an HTML tree image and you will finish the missing code within the string html which produces this HTML tree.
+
+html_tree_exercise.png
+
+To note:
+
+We have started the string html for you, to help nudge you in the correct direction.
+The spacing we use in the portion of the string we provide (e.g., indenting <head> two spaces more than <html>) isn't necessary, but we did so just to make it easier to read.
+
+Instructions
+100 XP
+Fill in the blank within the string variable html so that the HTML code matches its tree representation (including the text within the two paragraph children).
+'''
+
+html = '''
+<html>
+  <head>
+    <title>Intro HTML</title>
+  </head>
+  <body>
+  <p>Hello World!</p>
+  <p>Enjoy DataCamp</p>
+  </body>
+</html>
+'''
